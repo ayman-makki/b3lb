f5df8a80190cd17b1131c4a81a6c5b5bb17d6c82
Ayman Makki <manaykimak@gmail.com>
2025-11-21 07:57:44 +0100

    added LOGGING configuration to DJango

--- b3lb/loadbalancer/settings.py
+++ b3lb/loadbalancer/settings.py
@@ -194,3 +194,38 @@ vars().update(EMAIL_CONFIG)
 
 # set Django admins for error notification
 ADMINS = email.utils.getaddresses([env.str('DJANGO_ADMINS', '')])
+
+
+# Logging Configuration
+LOGGING = {
+    'version': 1,
+    'disable_existing_loggers': False,
+    'formatters': {
+        'verbose': {
+            'format': '{levelname} {asctime} {module} {message}',
+            'style': '{',
+        },
+        'simple': {
+            'format': '{levelname} {message}',
+            'style': '{',
+        },
+    },
+    'handlers': {
+        'console': {
+            'class': 'logging.StreamHandler',
+            'formatter': 'verbose',
+        },
+    },
+    'loggers': {
+        'rest': {
+            'handlers': ['console'],
+            'level': 'INFO',
+            'propagate': True,
+        },
+        'django': {
+            'handlers': ['console'],
+            'level': 'INFO',
+            'propagate': False,
+        },
+    },
+}
